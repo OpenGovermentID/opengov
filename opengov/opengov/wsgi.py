06cac4fe87d5ad1395b66311cb78be0b1bd01186
drayanaindra <masepindrayana@gmail.com>
2014-06-26 20:54:09 +0700

    first push

---
+++ opengov/opengov/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for opengov project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opengov.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
