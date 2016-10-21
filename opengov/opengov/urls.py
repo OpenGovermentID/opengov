eb62c85c8fe46a9ff9127466414864b0d5417ef1
ridwanfajar <ridwanbejo@gmail.com>
2016-10-21 08:23:54 +0700

    add several urls for hospital. segmentation for ehealth layout

--- opengov/opengov/urls.py
+++ opengov/opengov/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 admin.autodiscover()
 
-from ehealt import views as ehealth_views
+from ehealt.views import hospital as hospital_views
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,5 +13,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^health/', ehealth_views.index),
+    url(r'^health/hospital/$', hospital_views.index),
+    url(r'^health/hospital/add$', hospital_views.add),
+    url(r'^health/hospital/(?P<hospital_id>\w+)$', hospital_views.detail),
+    url(r'^health/hospital/edit/(?P<hospital_id>\w+)$', hospital_views.edit),
+    url(r'^health/hospital/delete/(?P<hospital_id>\w+)$', hospital_views.delete),
 )
